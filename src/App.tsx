770353b27e589ec47a8de46edf34837794d9f518
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2023-10-19 15:05:32 +0600

    refactoring tab and config eslint file

--- src/App.tsx
+++ src/App.tsx
@@ -3,47 +3,45 @@ import Button from './components/Buttons/Button';
 import Circle from './components/Circle/Circle';
 
 const App = () => {
-    const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+  const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
 
-    const newNumbers = (): void => {
-        const newNumbers: number[] = [];
+  const newNumbers = (): void => {
+    const newNumbers: number[] = [];
 
-        while (newNumbers.length < 5) {
-            const newNumber = Math.ceil(Math.random() * (36 - 5 + 1)) + 5;
-            if (!newNumbers.includes(newNumber)) {
-                newNumbers.push(newNumber);
-            }
+    while (newNumbers.length < 5) {
+      const newNumber = Math.ceil(Math.random() * (36 - 5 + 1)) + 5;
+      if (!newNumbers.includes(newNumber)) {
+        newNumbers.push(newNumber);
+      }
+    }
+
+    for (let i = 0; i < newNumbers.length - 1; i++) {
+      for (let j = i + 1; j < newNumbers.length; j++) {
+        if (newNumbers[i] > newNumbers[j]) {
+          const oldNumber = newNumbers[i];
+          newNumbers[i] = newNumbers[j];
+          newNumbers[j] = oldNumber;
         }
+      }
+    }
 
-        for (let i = 0; i < newNumbers.length - 1; i++) {
-            for (let j = i + 1; j < newNumbers.length; j++) {
-                if (newNumbers[i] > newNumbers[j]){
-                    const oldNumber = newNumbers[i];
-                    newNumbers[i] = newNumbers[j];
-                    newNumbers[j] = oldNumber;
-                }
-            }
+    setNumbers(newNumbers);
+  };
+
+  return (
+    <div className="text-center mt-[30px]">
+      <Button onclick={newNumbers}>New numbers</Button>
+      <div className="flex justify-center">
+        {
+          numbers.map((number: number, index: number) => {
+            return (
+              <Circle key={index} number={number}/>
+            );
+          })
         }
-
-        setNumbers(newNumbers);
-    };
-
-
-
-    return (
-        <div className='text-center mt-[30px]'>
-            <Button onclick={newNumbers}>New numbers</Button>
-            <div className='flex justify-center'>
-                {
-                    numbers.map((number: number, index: number) => {
-                        return (
-                            <Circle key={index} number={number}/>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Circle/Circle.tsx
+++ src/components/Circle/Circle.tsx
@@ -1,9 +1,10 @@
 const Circle = ({number}) => {
-    return (
-        <div className={`w-[50px] mt-[20px] mr-[15px] h-[50px] rounded-[50px] justify-center text-white items-center flex bg-red-400`}>
-            {number}
-        </div>
-    )
+  return (
+    <div
+      className={`w-[50px] mt-[20px] mr-[15px] h-[50px] rounded-[50px] justify-center text-white items-center flex bg-red-400`}>
+      {number}
+    </div>
+  );
 };
 
 export default Circle;
