a95f44cd3581ce03e65a413e3ce91542f5a6d6ab
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2023-10-19 14:44:47 +0600

    config tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
 import React, {useState} from 'react';
 import Button from './components/Buttons/Button';
+import Circle from './components/Circle/Circle';
 
 const App = () => {
-    const [numbers, setNumbers] = useState([5, 11, 16, 23, 32]);
+    const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
 
-    const newNumbers = () => {
-        setNumbers([4,23,45,67,78]);
+    const newNumbers = (): void => {
+        const newNumbers: number[] = [];
+
+        while (newNumbers.length < 5) {
+            const newNumber = Math.ceil(Math.random() * (36 - 5 + 1)) + 5;
+            if (!newNumbers.includes(newNumber)) {
+                newNumbers.push(newNumber);
+            }
+        }
+
+        for (let i = 0; i < newNumbers.length - 1; i++) {
+            for (let j = i + 1; j < newNumbers.length; j++) {
+                if (newNumbers[i] > newNumbers[j]){
+                    const oldNumber = newNumbers[i];
+                    newNumbers[i] = newNumbers[j];
+                    newNumbers[j] = oldNumber;
+                }
+            }
+        }
+
+        setNumbers(newNumbers);
     };
 
 
-    return(
-        <div>
+
+    return (
+        <div className='text-center'>
             <Button onclick={newNumbers}>New numbers</Button>
+            <div className='flex justify-center'>
+                {
+                    numbers.map((number: number, index: number) => {
+                        return (
+                            <Circle key={index} number={number}/>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 };
